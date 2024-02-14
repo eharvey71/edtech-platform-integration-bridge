e7a66ef6b356166f9d76ad2f2566aeef57fa2c51
Eric Harvey <planetside0@gmail.com>
2024-02-13 22:03:47 -0500

    more refactoring and docs setup

--- build_database.py
+++ build_database.py
@@ -7,85 +7,84 @@ from models import Note, KalturaAppToken, User, AccessRestrictions, \
 
 SAMPLE_TOKENS = [
     {
-        "kaltura_token_id": "1_1p5x2pyk",
-        "token": "0a8c24ddc480a37ed130fda312b47503",
+        "kaltura_token_id": "1_06ukdpod",
+        "token": "743659feab72a7b9393bedc0sfcbf156",
+        "partnerId": 12312312,
+        "createdAt": 1688050262,
+        "updatedAt": 1688050262,
+        "status": 2,
+        "expiry": 1767157200,
+        "sessionDuration": 86400,
+        "sessionPrivileges": "privacycontext:lceducation",
+        "hashType": "SHA256",
+        "objectType": "KalturaAppToken",
+        "notes": [
+            ("Using Privacy Context for specific Canvas courses", "2022-03-05 22:18:10"),
+        ],
+    },
+    {
+        "kaltura_token_id": "1_38xyqzs6",
+        "token": "f047ddf1f144514ec0db9a587a312d8c",
         "partnerId": 4526213,
+        "createdAt": 1690220095,
+        "updatedAt": 1690220095,
+        "status": 2,
+        "expiry": 1767157200,
+        "sessionUserId": "teacher1@university.edu",
+        "sessionDuration": 86400,
+        "sessionPrivileges": "enableentitlement,widget:1,urirestrict:/api_v3/service/media/*,/api_v3/service/caption_captionasset/*",
+        "hashType": "SHA256",
+        "objectType": "KalturaAppToken",
+        "label": "ANTH-201-01",
+        "notes": [
+            ("This token has access limited to media and caption endpoints", "2022-03-05 22:18:10"),
+        ],
+    },
+    {
+        "kaltura_token_id": "1_1p5x2pya",
+        "token": "0a8c24ddc480a37ed130fda312b47123",
+        "partnerId": 12312312,
         "createdAt": 1666634128,
         "updatedAt": 1666634128,
         "status": 2,
         "sessionType": 0,
-        "expiry": 1703980800,
+        "expiry": 1767157200,
         "sessionDuration": 86400,
-        "sessionUserId": "planetside0+kaltura@gmail.com",
+        "sessionUserId": "teacher@university.edu",
         "sessionPrivileges": "setrole:25448593,editadmintags:*",
         "description": '{"type": "kalturaTESTAppToken", "version": "1.0.0"}',
         "label": "CIS10101-Test",
         "notes": [
             ("This token was created for access to ARTH-101 course videos.", "2022-01-06 17:10:24"),
-            ("Updated", "2022-03-05 22:18:10"),
+            ("Updated label", "2022-03-05 22:18:10"),
         ],
     },
-    {
-        "kaltura_token_id": "1_06ukdvod",
-        "token": "743659feab72a7b9393bedc0fecbf156",
-        "partnerId": 4526213,
-        "createdAt": 1688050262,
-        "updatedAt": 1688050262,
-        "status": 2,
-        "expiry": 1703980800,
-        "sessionDuration": 86400,
-        "sessionPrivileges": "setrole:28239382,privacycontext:lceducation,learningclues",
-        "hashType": "SHA256",
-        "objectType": "KalturaAppToken",
-        "notes": [
-            ("Updated this one third", "2022-03-05 22:18:10"),
-        ],
-    },
-    {
-        "kaltura_token_id": "1_38xyqzr6",
-        "token": "f047ddf1f144514ec0db9a511a312d8c",
-        "partnerId": 4526213,
-        "createdAt": 1690220095,
-        "updatedAt": 1690220095,
-        "status": 2,
-        "expiry": 1703980800,
-        "sessionUserId": "planetside0+kms@gmail.com",
-        "sessionDuration": 86400,
-        "sessionPrivileges": "enableentitlement,setrole:12579732,widget:1,urirestrict:/api_v3/service/media/*,/api_v3/service/caption_captionasset/*",
-        "hashType": "SHA256",
-        "objectType": "KalturaAppToken",
-        "label": "ANTH-201-01",
-        "notes": [
-            ("Updated this second", "2022-03-05 22:18:10"),
-        ],
-    }
 ]
 
 SAMPLE_USERS = [
     {
-        "username": "eharvey",
+        "username": "admin",
         "password": "pbkdf2:sha256:600000$7U76bfR0B52uFkSN$5270c986f23025fea3e55e5035da66910de3d71d85c996de87d04a564bed15e7",
-        "email": "planetside0@gmail.com",
+        "email": "admin@enwiseweb.com",
         "role": "admin"
     },
     {
         "username": "developer",
         "password": "pbkdf2:sha256:600000$7U76bfR0B52uFkSN$5270c986f23025fea3e55e5035da66910de3d71d85c996de87d04a564bed15e7",
-        "email": "planetside0+dev@gmail.com",
+        "email": "developer@company.com",
         "role": "developer"
     }
 ]
 
 SAMPLE_CONFIG = [
     {
-        "allowed_categories": "594123,634123",
-        "label_mapping": "1_c9w0xr3=CIS101,1_29dxy09=PHAR30102"
+        "allowed_categories": "594123,634123"
     }
 ]
 
 SAMPLE_SESSION_DEFAULTS = [
     {
-        "partner_id": 4526213,
+        "partner_id": 12121212,
         "session_expiry": 86400,
         "use_local_storage": True
     }
@@ -128,17 +127,17 @@ with app.app_context():
         db.session.add(new_user)
     new_cat_restrict = AccessRestrictions(
         allowed_categories=SAMPLE_CONFIG[0].get("allowed_categories"),
-        force_labels=True
+        force_labels=False
     )
     db.session.add(new_cat_restrict)
     new_app_token_session_defaults = AppTokenSessionDefaults(
         partner_id=SAMPLE_SESSION_DEFAULTS[0].get("partner_id"),
         session_expiry=SAMPLE_SESSION_DEFAULTS[0].get("session_expiry"),
-        use_local_storage=True
+        use_local_storage=False
     )   
     db.session.add(new_app_token_session_defaults)
     new_ui_customizations = UICustomizations(
-        integrator_title="LearningClues Integration Manager"
+        integrator_title="EdTech Platform Integration Bridge"
     )
     db.session.add(new_ui_customizations)
     db.session.commit()
--- config.py
+++ config.py
@@ -17,7 +17,7 @@ connex_app = FlaskApp(__name__, specification_dir=basedir)
 app = connex_app.app
 
 app.config['DEBUG'] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'lc.db'}"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'database/epib.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'darkly'
 app.config['SECRET_KEY'] = '3d329dj239d0j2390df023f320f32d9023nd3290d230'
